8535fae7d3bfe696367109560d2aca5f19d95754
Cristina Bobadilla <claudia.bobadilla.2804@gmail.com>
2024-03-25 12:16:57 -0500

    feat: initial project setup
    - Configure project structure
    - Set up development environment
    - Add basic dependencies (Vue.js, Typescript, VueRouter, Pinia, AntDesign)
    - Initialize Git repository

---
+++ src/pinia.ts
@@ -0,0 +1,8 @@
+import { createPinia } from "pinia";
+import { App } from "vue";
+
+export const pinia = createPinia();
+
+export const installPinia = (app: App<Element>) => {
+  app.use(pinia);
+};
